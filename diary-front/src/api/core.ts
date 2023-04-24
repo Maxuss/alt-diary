a5067f40deb65d5dc8ec44f2f205afe5b41fa4e0
maxus <maxus.info.mail@gmail.com>
2023-04-24 23:18:01 +0300

    actually forgot about it

--- diary-front/src/api/core.ts
+++ diary-front/src/api/core.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-export const BASE_API_URL: string = "http://localhost:3000/api"
+export const BASE_API_URL: string = "http://dnevnik.maxus.space/api"
 
 export async function post(url: string, body: any = {}): Promise<any> {
     return await axios.post(BASE_API_URL + url, {
